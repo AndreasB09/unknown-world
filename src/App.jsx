cceabbf1fe72a4d336bcc413f5ef99571c45caa2
andreasb09 <andreasb09.kodehode@gmail.com>
2024-03-21 12:27:37 +0100

    layout and routing start

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import Home from './pages/Home/Home.jsx'
+import { Route } from 'react-router-dom';
 
 function App() {
 
