bd4008f11b126575aeea5edac860ef981c56cf36
andreasb09 <andreasb09.kodehode@gmail.com>
2024-03-29 13:33:06 +0100

    img error

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
 import styles from './Footer.module.css';
 //import githubLogo from '../../assets/ghithub_logo.svg';
-// import decidiousTree from '../../assets/decidious-tree.svg'
+//import decidiousTree from '../../assets/decidious-tree.svg'
 
 function Footer() {
     return (
         <footer className={styles.container}>
-            <img src={githubLogo}></img>
+            {/* <img src={githubLogo} alt="githublogo" /> */}
         </footer>
     )
 }
