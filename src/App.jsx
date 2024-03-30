37133da233a79864bd911ad6affa57293f41aba4
andreasb09 <andreasb09.kodehode@gmail.com>
2024-03-30 13:00:13 +0100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 import './App.css'
-import Header from './components/Header/Header.jsx'
-import Footer from './components/Footer/Footer.jsx'
+//import Header from './components/Header/Header.jsx'
+//import Footer from './components/Footer/Footer.jsx'
 import Home from './pages/Home/Home.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FactionOne from './pages/FactionOne/FactionOne.jsx';
