1669553547167e2ae226dc744a0df43e82d7b331
andreasb09 <andreasb09.kodehode@gmail.com>
2024-04-25 14:06:39 +0200

    fail

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
 import styles from './Footer.module.css';
 // import githubLogo from '../../assets/ghithub_logo.svg';
-import decidiousTree from '../../assets/decidious-tree.svg';
+// import decidiousTree from '../../assets/decidious-tree.svg';
 
 function Footer() {
     return (
         <footer className={styles.container}>
-            <img src={decidiousTree} alt="githublogo" />
+            {/* <img src={decidiousTree} alt="githublogo" /> */}
         </footer>
     )
 }
