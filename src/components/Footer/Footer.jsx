76012bab1ab443462095dc3a44cc9cfec942d68b
andreasb09 <andreasb09.kodehode@gmail.com>
2024-04-26 11:02:38 +0200

    imgs working

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,14 @@
 import styles from './Footer.module.css';
-// import githubLogo from '../../assets/ghithub_logo.svg';
-// import decidiousTree from '../../assets/decidious-tree.svg';
+import githubLogo from '../../assets/github_logo.svg';
+import deciduousTree from '../../assets/deciduous-tree.svg';
 
 function Footer() {
     return (
-        <footer className={styles.container}>
-            {/* <img src={githubLogo} alt="githublogo" /> */}
-        </footer>
-    )
+      <footer className={styles.container}>
+        <img src={githubLogo} alt="githublogo" />
+        <img src={deciduousTree} alt="aTree" />
+      </footer>
+    );
 }
 
 export default Footer
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import Layout from '../../components/Layout/Layout';
 import styles from './Home.module.css';
+import deciduousTree from "../../assets/deciduous-tree.svg";
+import githubLogo from "../../assets/github_logo.svg";
 
 function Home() {
     return (
       <Layout>
         <div className={styles.container}>
-          <div className={styles.square}></div>
+          <div className={styles.square}>
+            <img src={deciduousTree} alt="aTree" />
+            <img src={githubLogo} alt="logoOfGithub" />
+          </div>
         </div>
       </Layout>
     );
